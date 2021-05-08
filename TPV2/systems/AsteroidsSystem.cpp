3eb643b01704533527ee2a0d1212b3e2b47defb0
Ivan Prado <ivprado@ucm.es>
2021-05-08 12:29:44 +0200

    movidas y sistemas varios

---
+++ TPV2/systems/AsteroidsSystem.cpp
@@ -0,0 +1,13 @@
+#include "AsteroidsSystem.h"
+
+void AsteroidsSystem::addAsteroids(int n)
+{
+}
+
+void AsteroidsSystem::onCollisionWithBullet(Entity* a, Entity* b)
+{
+}
+
+void AsteroidsSystem::update()
+{
+}
---
+++ TPV2/systems/CollisionsSystem.cpp
@@ -0,0 +1,5 @@
+#include "CollisionsSystem.h"
+
+void CollisionSystem::update()
+{
+}
